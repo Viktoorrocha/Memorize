4b9b4cd56c6b1a9fd87e54c544f5085600a9c740
Viktor <viktoorrocha@gmail.com>
2021-03-28 21:07:41 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Viktor Rocha on 28/03/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
